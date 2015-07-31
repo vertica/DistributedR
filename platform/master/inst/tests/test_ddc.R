f09b93ad883cd3fdbb4f76260db008bbb3e5ef5c
Jorge Martinez <jorge.marsal@gmail.com>
2015-07-31 14:57:44 -0700

    Add simple unit test for DDC.

---
+++ platform/master/inst/tests/test_ddc.R
@@ -0,0 +1,26 @@
+test_that("ex001.csv", {
+  df <- distributedR_read(paste(getwd(),'/data/ex001.csv',sep=''), schema='a:int64,b:string')
+  localdf <- getpartition(df)
+  expect_true(is.data.frame(localdf))
+  expect_equal(nrow(localdf), 3)
+  expect_equal(ncol(localdf), 2)
+})
+
+test_that("ex002.csv", {
+  df <- distributedR_read(paste(getwd(),'/data/ex002.csv',sep=''), schema='a:int64,b:string,c:int64,d:string')
+  localdf <- getpartition(df)
+  expect_true(is.data.frame(localdf))
+  expect_equal(nrow(localdf), 3)
+  expect_equal(ncol(localdf), 4)
+})
+
+test_that("TestOrcFile.test1.orc", {
+  df <- distributedR_read(paste(getwd(),'/data/TestOrcFile.test1.orc',sep=''), selectedStripes='0')
+  localdf <- getpartition(df)
+  expect_true(is.data.frame(localdf))
+  expect_equal(nrow(localdf), 2)
+  expect_equal(ncol(localdf), 12)
+})
+
+
+
