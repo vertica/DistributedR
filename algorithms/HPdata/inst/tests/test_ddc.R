37e54aa0fe47f599a06e84af59ab4287f7979f3d
Jorge Martinez <jorge.marsal@gmail.com>
2015-08-14 10:55:25 -0700

    Move DDC unit tests to HPdata package.

--- algorithms/HPdata/inst/tests/test_ddc.R
+++ algorithms/HPdata/inst/tests/test_ddc.R
@@ -1,5 +1,8 @@
+library(HPdata)
+distributedR_start()
+
 test_that("ex001.csv", {
-  df <- distributedR_read(paste(getwd(),'/data/ex001.csv',sep=''), schema='a:int64,b:string')
+  df <- csv2dframe(paste(getwd(),'/data/ex001.csv',sep=''), schema='a:int64,b:string')
   localdf <- getpartition(df)
   expect_true(is.data.frame(localdf))
   expect_equal(nrow(localdf), 128)
@@ -7,7 +10,7 @@ test_that("ex001.csv", {
 })
 
 test_that("ex002.csv", {
-  df <- distributedR_read(paste(getwd(),'/data/ex002.csv',sep=''), schema='a:int64,b:string,c:int64,d:string')
+  df <- csv2dframe(paste(getwd(),'/data/ex002.csv',sep=''), schema='a:int64,b:string,c:int64,d:string')
   localdf <- getpartition(df)
   expect_true(is.data.frame(localdf))
   expect_equal(nrow(localdf), 128)
@@ -15,7 +18,7 @@ test_that("ex002.csv", {
 })
 
 test_that("TestOrcFile.test1.orc", {
-  df <- distributedR_read(paste(getwd(),'/data/TestOrcFile.test1.orc',sep=''), selectedStripes='0')
+  df <- orc2dframe(paste(getwd(),'/data/TestOrcFile.test1.orc',sep=''), selectedStripes='0')
   localdf <- getpartition(df)
   expect_true(is.data.frame(localdf))
   expect_equal(nrow(localdf), 2)
