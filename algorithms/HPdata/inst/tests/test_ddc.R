4bbb06c8c2966ca65715586471c2ca6aacfac9fb
Jorge Martinez <jms@hpe.com>
2015-09-29 07:48:43 -0700

    Add context() at the beginning of ddc tests.

--- algorithms/HPdata/inst/tests/test_ddc.R
+++ algorithms/HPdata/inst/tests/test_ddc.R
@@ -1,5 +1,7 @@
 library(HPdata)
 
+context("Distributed Data Connector")
+
 test_that("ex001.csv", {
   df <- csv2dframe(paste(getwd(),'/data/ex001.csv',sep=''), schema='a:int64,b:string')
   localdf <- getpartition(df)
