9a530f3425131886f6959c06460c28b87ae30f45
Arash-VM <arash.fard@hp.com>
2015-10-22 11:31:04 -0400

    I am not sure when and how the vote vector had been corrupted. I found that from the failiour in a regression test [parallelForestInOut.R]

--- algorithms/HPdclassifier/R/hpdRF_parallelForest.R
+++ algorithms/HPdclassifier/R/hpdRF_parallelForest.R
@@ -202,7 +202,7 @@
     inputData <- list(ntree=ntree, mtry=mtry, 
             replace=replace, classwt=classwt, sampsize=sampsize,
             nodesize=nodesize, maxnodes=maxnodes, importance=importance, localImp=localImp,
-            nPerm=nPerm, proximity=proximity,
+            nPerm=nPerm, proximity=proximity, norm.votes=FALSE,
             keep.forest=keep.forest, corr.bias=corr.bias, nExecutor=nExecutor)
     # these arguments don't have default values in the original signature of the function
     if (!missing(cutoff))
